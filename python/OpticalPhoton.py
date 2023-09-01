e2a5adb9d4b2d85c68f33748915d28d8209377ca
acolijn <auk.pieter.colijn@gmail.com>
2023-09-01 17:04:23 +0200

    made OpticalPhoton class

---
+++ python/OpticalPhoton.py
@@ -0,0 +1,24 @@
+class OpticalPhoton:
+    """
+    Class to define a photon and propagate it through a simplified XENON detector
+    
+    A.P. Colijn
+    """
+    def __init__(self, **kwargs):
+        """
+
+        A.P. Colijn
+        """
+    
+    def intersect_with_plane(self):
+
+        return 0
+    
+    def intersect_with_cylinder(self):
+
+        return 0
+
+    
+    def propagate(self):
+
+        return 0
